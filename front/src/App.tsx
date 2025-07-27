b4583ba1e7b98cb4b34255d0f3915c595f98e4e6
한영균 <hyg9112@naver.com>
2025-07-27 22:43:31 +0900

    feat(header):헤더 구현 및 폰트 컬러 팔레트 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,16 @@
+import Header from './components/Header';  // 사이드 헤더
 import FeedCard from './components/feed/FeedCard';
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-[#F8F9FA]">
-      <FeedCard />
+    <div className="flex min-h-screen bg-[#F8F9FA]">
+      {/* 좌측 헤더 */}
+      <Header />
+
+      {/* 우측 콘텐츠 영역 */}
+      <main className="flex-1 flex justify-center items-center p-4">
+        <FeedCard />
+      </main>
     </div>
   );
 }
