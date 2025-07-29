eff111079c297b652e883f5879d43ffa6ca1f614
AhiamYeong <toktto@gmail.com>
2025-07-29 19:23:13 +0900

    tmp: codeList 컴포넌트 정리
    Former-commit-id: 17a65d1104673f9e72d3a9f34f8817ce385c0308

--- front/src/pages/NoteGenerate.tsx
+++ front/src/pages/NoteGenerate.tsx
@@ -3,6 +3,7 @@ import CodeList from '../components/code/CodeList';
 <<<<<<< HEAD
 import CodeEditor from '../components/code/CodeEditor';
 import CodePreview from '../components/code/CodePreview';
+<<<<<<< HEAD
 import type { SubmissionItem } from '../types';
 import { useState } from 'react';
 import mockSubmissionApiResponse from '../data/MockSubmissionData';
@@ -86,8 +87,29 @@ export default function NoteGenerate() {
 import CodeEditor from '../components/code/CodeEditor';
 import CodePreview from '../components/code/CodePreview';
 >>>>>>> 458739a (style(note): 코드 생성 페이지 레이아웃 초기 설정)
+=======
+import mockList from '../data/MockSubmitList';
+import type { SubmissionItem } from '../types';
+import { useState } from 'react';
+>>>>>>> 1fd35ed (tmp: codeList 컴포넌트 정리)
 
 export default function NoteGenerate() {
+  const [successCode, setSuccessCode] = useState<SubmissionItem | null>(null);
+  //   const [failCode, setFailCode] = useState<SubmissionItem | null>(null);
+
+  //   TODO: mock 코드 성공 / 실패 따로 변경
+  // TODO: fetch 변경 & 비동기 로직 추가
+  const successCodeList: SubmissionItem[] = mockList.filter(
+    (item) => item.resultText === '맞았습니다!!',
+  );
+  const failCodeList: SubmissionItem[] = mockList.filter(
+    (item) => item.resultText !== '맞았습니다!!',
+  );
+
+  //   확장성 증가 위해 함수로 감쌈
+  const handleCodeChange = (submission: SubmissionItem) => {
+    setSuccessCode(submission);
+  };
   return (
     <div className="note-generate-page">
       <div className="header bg-blue-700">문제이름</div>
@@ -98,11 +120,18 @@ export default function NoteGenerate() {
         <div className="code-container basis-2/3">
           {/* 제출내역 list */}
           <div className="code-list flex">
+            {/* TODO: mockData success/fail 분해 */}
             <div className="success-code basis-1/2">
-              <CodeList></CodeList>
+              <CodeList
+                list={successCodeList}
+                onCodeSelect={handleCodeChange}
+              ></CodeList>
             </div>
             <div className="fail-code basis-1/2">
-              <CodeList></CodeList>
+              <CodeList
+                list={failCodeList}
+                onCodeSelect={handleCodeChange}
+              ></CodeList>
             </div>
           </div>
 
@@ -114,16 +143,20 @@ export default function NoteGenerate() {
           <div className="code-preview flex">
             <div className="success-code basis-1/2">
               <h2>성공코드</h2>
-              <CodePreview
-                code="success-code-test"
-                language="python3"
-              ></CodePreview>
+              {/* <CodePreview
+                code={successCode?.code}
+                language={successCode?.language}
+              ></CodePreview> */}
             </div>
             <div className="fail-code basis-1/2">
               <h2>실패코드</h2>
-              <CodePreview code="fail-code-test" language="java"></CodePreview>
+              {/* <CodePreview
+                code={failCode?.code}
+                language={failCode?.language}
+              ></CodePreview> */}
             </div>
           </div>
+          {/* TODO: GPT 생성 API 연결 */}
           <button className="create-btn">코드 생성</button>
         </div>
 
@@ -131,6 +164,8 @@ export default function NoteGenerate() {
         <div className="editor-container basis-1/3">
           <CodeEditor></CodeEditor>
           <div>공개 범위 설정</div>
+
+          {/* TODO: 생성 API 연결 */}
           <button>저장하기</button>
         </div>
       </div>
