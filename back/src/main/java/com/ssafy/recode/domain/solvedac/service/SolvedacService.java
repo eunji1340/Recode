982659af466da636f652116bbb376a4c9eff890c
yujinoh <0rangesunset@naver.com>
2025-08-01 13:40:17 +0900

    feat: solovedac service 구현중
    Former-commit-id: 2335bccfda0085c0d34de412a36688078faa219b

--- back/src/main/java/com/ssafy/recode/domain/solvedac/service/SolvedacService.java
+++ back/src/main/java/com/ssafy/recode/domain/solvedac/service/SolvedacService.java
@@ -2,11 +2,12 @@ package com.ssafy.recode.domain.solvedac.service;
 
 import com.ssafy.recode.domain.solvedac.dto.SolvedacResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.http.HttpHeaders;
 import java.nio.charset.StandardCharsets;
 
 @Service
@@ -15,19 +16,21 @@ public class SolvedacService {
 
     private final RestTemplate restTemplate;
 
-//    public SolvedacResponseDto getSuggestions(String query){
-//        // URL 생성
-//        String url = UriComponentsBuilder
-//                .fromHttpUrl("https://solved.ac/api/v3/search/suggestion")
-//                .queryParam("query", query)
-//                .build()
-//                .encode(StandardCharsets.UTF_8)
-//                .toUriString();
-//
-//        // 요청 헤더 생성
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("");
-//
-//    }
+    public SolvedacResponseDto getSuggestions(String query){
+        // URL 생성
+        String url = UriComponentsBuilder
+                .fromHttpUrl("https://solved.ac/api/v3/search/suggestion")
+                .queryParam("query", query)
+                .build()
+                .encode(StandardCharsets.UTF_8)
+                .toUriString();
+
+        // 요청 헤더 생성: 응답 받을 언어 한국어로 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-solvedac-language", "ko");
+
+        // 요청 객체 생성
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+    }
 
 }
