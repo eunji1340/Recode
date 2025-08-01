a9d0e444719e4f31cf5898f4a22c9939fe66b53d
AhiamYeong <toktto@gmail.com>
2025-08-01 16:33:29 +0900

    tmp/react-markdown 라이브러리 설치 & tailwind 수정중
    Former-commit-id: bd753be7c58bc9e4c7f50c6f464a433d705c8834

--- front/src/components/code/CodeEditor.tsx
+++ front/src/components/code/CodeEditor.tsx
@@ -1,9 +1,25 @@
-// 코드 에디터
+import Markdown from 'react-markdown';
+import { useState, type SetStateAction } from 'react';
+
 export default function CodeEditor() {
+  const [markdown, setMarkdown] = useState('');
+
+  const handleInputChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setMarkdown(event.target.value);
+  };
+
   return (
     <div>
-      <h2>에디터</h2>
-      <div>코드에디터부분 마크다운마크다운</div>
+      <textarea
+        value={markdown}
+        onChange={handleInputChange}
+        style={{ width: '50%', height: '300px' }}
+      />
+      <div>
+        <Markdown>{markdown}</Markdown>
+      </div>
     </div>
   );
 }
