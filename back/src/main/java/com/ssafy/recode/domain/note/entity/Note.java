510460afb847f4e066c125935ba1d626dda496c7
DESKTOP-DLOPF19\SSAFY <kelsey6225@naver.com>
2025-08-05 14:04:58 +0900

    fix: 엔티티 수정
    Former-commit-id: 5dc959b0da433f9e7d01a6e9a5f73c0f12a8cf68

--- back/src/main/java/com/ssafy/recode/domain/note/entity/Note.java
+++ back/src/main/java/com/ssafy/recode/domain/note/entity/Note.java
@@ -32,7 +32,7 @@ public class Note {
 
     @Lob
     private String content;
-
+    @Column(columnDefinition = "LONGTEXT")
     private String successCode;
     private Integer successCodeStart;
     private Integer successCodeEnd;
@@ -42,6 +42,7 @@ public class Note {
 
     private Integer failCodeStart;
     private Integer failCodeEnd;
+    @Column(columnDefinition = "LONGTEXT")
     private String failCode;
 
     @Column(name = "fail_language", length = 20)
