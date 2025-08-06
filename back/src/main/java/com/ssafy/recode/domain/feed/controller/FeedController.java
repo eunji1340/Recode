8f19f0f9045c7f18f4942b7c997dc94efb0c2382
DESKTOP-DLOPF19\SSAFY <kelsey6225@naver.com>
2025-08-06 14:19:16 +0900

    add: swagger 설명 추가
    Former-commit-id: 9e64fe6377e90b3b43ac17fd52a6b53f307738e6

--- back/src/main/java/com/ssafy/recode/domain/feed/controller/FeedController.java
+++ back/src/main/java/com/ssafy/recode/domain/feed/controller/FeedController.java
@@ -8,6 +8,7 @@ import com.ssafy.recode.domain.feed.service.FeedService;
 import com.ssafy.recode.global.dto.response.ApiListPagingResponse;
 import com.ssafy.recode.global.dto.response.ApiListResponse;
 import com.ssafy.recode.global.dto.response.ApiSingleResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -33,6 +34,7 @@ public class FeedController {
 
     // 좋아요 수 조회
     @GetMapping("/{noteId}/hearts")
+    @Operation(summary = "특정 노트의 좋아요 수 조회")
     public ResponseEntity<ApiSingleResponse<Map<String, Integer>>> getLikeCount(@PathVariable Long noteId) {
         int count = feedService.getLikeCount(noteId);
         Map<String, Integer> response = new HashMap<>();
@@ -42,6 +44,7 @@ public class FeedController {
 
     // 좋아요 추가
     @PostMapping("/{noteId}/hearts")
+    @Operation(summary = "특정 노트에 좋아요 추가")
     public ResponseEntity<Long> addLike(@AuthenticationPrincipal CustomUserDetails userDetails,
                                         @PathVariable Long noteId) {
         Long userId = userDetails.getUser().getUserId();
@@ -51,6 +54,7 @@ public class FeedController {
 
     // 좋아요 삭제
     @DeleteMapping("/{noteId}/hearts/{likeId}")
+    @Operation(summary = "특정 노트에 좋아요 삭제")
     public ResponseEntity<Void> removeLike(@PathVariable Long noteId,
                                            @PathVariable Long likeId) {
         feedService.removeLike(likeId);
@@ -59,6 +63,7 @@ public class FeedController {
 
     // 댓글 생성
     @PostMapping("/{noteId}/comments")
+    @Operation(summary = "특정 노트에 댓글 생성")
     public ResponseEntity<ApiSingleResponse<CommentResponseDto>> createComment(
             @PathVariable Long noteId,
             @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -72,6 +77,7 @@ public class FeedController {
 
     // 댓글 수 조회
     @GetMapping("/{noteId}/comments_count")
+    @Operation(summary = "특정 노트의 댓글 수 조회")
     public ResponseEntity<Integer> getCommentsCount(@PathVariable Long noteId) {
         int count = feedService.getCommentCount(noteId);
         return ResponseEntity.ok(count);
@@ -80,6 +86,7 @@ public class FeedController {
 
     // 댓글 조회
     @GetMapping("/{noteId}/comments")
+    @Operation(summary = "특정 노트의 전체 댓글 조회")
     public ResponseEntity<ApiListResponse<CommentResponseDto>> getComments(@PathVariable Long noteId) {
         List<CommentResponseDto> comments = feedService.getComments(noteId);
         return ResponseEntity.ok(ApiListResponse.from(comments));
@@ -87,6 +94,7 @@ public class FeedController {
 
     // 댓글 수정 -> 200 ok는 나오는데 content 이상하게 null로 나와요!!
     @PatchMapping("/{noteId}/comments/{commentId}")
+    @Operation(summary = "특정 노트의 댓글 수정")
     public ResponseEntity<ApiSingleResponse<CommentResponseDto>> updateComment(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                                                @PathVariable Long noteId,
                                                                                @PathVariable Long commentId,
@@ -98,6 +106,7 @@ public class FeedController {
 
     // 댓글 삭제
     @DeleteMapping("/{noteId}/comments/{commentId}")
+    @Operation(summary = "특정 노트의 댓글 삭제")
     public ResponseEntity<Void> deleteComment(@AuthenticationPrincipal CustomUserDetails userDetails,
                                               @PathVariable Long noteId,
                                               @PathVariable Long commentId) throws AccessDeniedException {
@@ -107,6 +116,7 @@ public class FeedController {
     }
 
     @GetMapping
+    @Operation(summary = "전체피드 조회 (태그/검색어)로 검색")
     public ResponseEntity<ApiListPagingResponse<FeedResponseDto>> getAllFeeds(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "15") int size,
@@ -125,6 +135,7 @@ public class FeedController {
     }
 
     @GetMapping("/followings")
+    @Operation(summary = "팔로우한 피드 조회 (태그/검색어)로 검색")
     public ResponseEntity<ApiListPagingResponse<FeedResponseDto>> getFeedsOfFollowings(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam(defaultValue = "0") int page,
