790503f09a9a1c581a70822d4c225700353e02d7
yujinoh <0rangesunset@naver.com>
2025-08-07 20:26:15 +0900

    fix: 백준 쿠키 컨트롤러에 swagger 설명 추가
    Former-commit-id: 75e4864ed695e52bb61a12c028e31da86ef35cb1

--- back/src/main/java/com/ssafy/recode/domain/cookie/controller/BaekjoonCookieController.java
+++ back/src/main/java/com/ssafy/recode/domain/cookie/controller/BaekjoonCookieController.java
@@ -3,6 +3,7 @@ package com.ssafy.recode.domain.cookie.controller;
 import com.ssafy.recode.auth.CustomUserDetails;
 import com.ssafy.recode.domain.cookie.dto.request.BaekjoonCookieRequestDto;
 import com.ssafy.recode.domain.cookie.service.BaekjoonCookieService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class BaekjoonCookieController {
 
     private final BaekjoonCookieService baekjoonCookieService;
 
-    // bojautologin 쿠키 저장
+    @Operation(summary = "백준 쿠키 저장", description = "백준 쿠키를 저장합니다.")
     @PostMapping
     public ResponseEntity<Void> saveCookie(@RequestBody BaekjoonCookieRequestDto dto,
                                            @AuthenticationPrincipal CustomUserDetails userDetails){
