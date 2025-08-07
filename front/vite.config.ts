12b815ccf9ccab0cb7a4770e50ed66ce68b8e1f6
AhiamYeong <toktto@gmail.com>
2025-08-08 01:42:41 +0900

    tmp/docker 로컬에 테스트중
    Former-commit-id: 34a8c5c75f3697f95cb49eedfc31831942d800bd

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 0.0.0.0 binding
+    port: 5173,
+    strictPort: true, // port 충돌시 실패
+  },
 });
